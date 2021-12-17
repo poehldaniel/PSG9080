b796763566cfae2cc4c2ce408fb4f685d9d7cf46
Daniel Pöhl <poehldaniel@gmail.com>
2021-12-17 10:10:19 +0100

    asserts and docstring

--- psg9080.py
+++ psg9080.py
@@ -1,3 +1,9 @@
+######################################################################
+# Author:      DANIEL PÖHL
+# Description: Framework to control the Programmable Signal Generator PSG9080
+# Comments:    last changes: 2021-12-17 10:06:35
+######################################################################
+
 import serial
 import codecs
 
@@ -35,30 +41,60 @@ class PSG9080:
     # function_code 0-99
 
     def __init__(self, port="COM7"):
+        """__init__ method
+
+        :param port: COM port of the PSG9080, defaults to "COM7"
+        :type port: str, optional
+        """
         self.port = port
         self.device = None
         self.channel_state = (0, 0)
 
-    def connect(self):
+    def connect(self) -> bool:
+        """connect to the selected COM port
+
+        :return: successfully connected (True) or not (False)
+        :rtype: bool
+        """
         try:
             self.device = serial.Serial(self.port, 115200, timeout=1)
             print(f"Connected to {self.port}")
+            return True
         except:
             print(f"Connection failed! ({self.port})")
-
-    def isConnected(self):
-        try:
-            return self.device.isOpen()
-        except:
             return False
 
-    def disconnect(self):
+    def disconnect(self) -> bool:
+        """disconnect the PSG9080
+
+        :return: successfully disconnected (True) or not (False)
+        :rtype: bool
+        """
         try:
             return self.device.close()
         except:
             return False
 
-    def __send(self, command):
+    def isConnected(self) -> bool:
+        """check if the connection to the PSG9080 is established
+
+        :return: connected (True) or not (False)
+        :rtype: bool
+        """
+        try:
+            return self.device.isOpen()
+        except:
+            return False
+
+    def __send(self, command: str) -> bool:
+        """send the desired command to the PSG9080
+
+        :param command: desired command 
+        :type command: str
+        :return: sending data successfully (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(command, str), "command must be a string"
         try:
             self.device.write(codecs.encode(command))
             response = self.device.readline()
@@ -69,10 +105,19 @@ class PSG9080:
         except:
             return False
 
-    def __receive(self):
+    def __receive(self) -> bool:
         pass
 
-    def on(self, channel=1):
+    def on(self, channel=1) -> bool:
+        """turn the selected channel on
+
+        :param channel: channel to be turned on, defaults to 1
+        :type channel: int, optional
+        :return: turned on successfully (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
         if channel == 1:
             self.channel_state = (1, self.channel_state[1])
         elif channel == 2:
@@ -80,16 +125,28 @@ class PSG9080:
         else:
             return False
         command = f":w10={self.channel_state[0]},{self.channel_state[1]}.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def on_all(self):
+    def on_all(self) -> bool:
+        """turning both channels on
+
+        :return: turned on successfully (True) or not (False)
+        :rtype: bool
+        """
         self.channel_state = (1, 1)
         command = f":w10=1,1.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def off(self, channel=1):
+    def off(self, channel=1) -> bool:
+        """turn the selected channel off
+
+        :param channel: channel to be turned off, defaults to 1
+        :type channel: int, optional
+        :return: turned off successfully (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
         if channel == 1:
             self.channel_state = (0, self.channel_state[1])
         elif channel == 2:
@@ -97,25 +154,57 @@ class PSG9080:
         else:
             return False
         command = f":w10={self.channel_state[0]},{self.channel_state[1]}.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def off_all(self):
+    def off_all(self) -> bool:
+        """turning both channels off
+
+        :return: turned off successfully (True) or not (False)
+        :rtype: bool
+        """
         self.channel_state = (0, 0)
         command = f":w10=0,0.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def waveform(self, channel=1, waveform="Sine"):
+    def waveform(self, channel=1, waveform="Sine") -> bool:
+        """select waveform
+
+        :param channel: desired channel, defaults to 1
+        :type channel: int, optional
+        :param waveform: desired waveform, defaults to "Sine"
+        :type waveform: str, optional
+        :return: waveform successfully selected (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
+        assert isinstance(waveform, str), "waveform must be a string"
+        assert waveform in self.waveforms, "waveform not known"
         # CH1: w11, CH2: w12
         if "Arbitrary" in waveform:
             command = f":w1{channel}={waveform[-3:]}.\n"
         else:
             command = f":w1{channel}={self.waveforms.index(waveform)}.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def frequency(self, channel=1, frequency=1000, unit="Hz"):
+    def frequency(self, channel=1, frequency=1000, unit="Hz") -> bool:
+        """select frequency
+
+        :param channel: desired channel, defaults to 1
+        :type channel: int, optional
+        :param frequency: desired frequency, defaults to 1000
+        :type frequency: int, optional
+        :param unit: unit of the frequency, defaults to "Hz"
+        :type unit: str, optional
+        :return: frequency successfully selected (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
+        assert isinstance(frequency, int), "frequency must be int"
+        units = ["uHz", "mHz", "Hz", "kHz", "MHz"]
+        assert isinstance(unit, str), "unit must be a string"
+        assert unit in units, f"unit not known, possible units: {units}"
         # CH1: w13, CH2: w14
         if unit == "Hz":
             unit_operator = 0
@@ -135,10 +224,26 @@ class PSG9080:
         else:
             return False
         command = f":w1{2+channel}={frequency},{unit_operator}.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def amplitude(self, channel=1, amplitude=1, unit="V"):
+    def amplitude(self, channel=1, amplitude=1.00, unit="V") -> bool:
+        """select amplitude
+
+        :param channel: desired channel, defaults to 1
+        :type channel: int, optional
+        :param amplitude: desired amplitude, defaults to 1
+        :type amplitude: int, optional
+        :param unit: unit of the amplitude, defaults to "V"
+        :type unit: str, optional
+        :return: amplitude successfully selected (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
+        assert isinstance(amplitude, float), "amplitude must be int or float"
+        units = ["mV", "V"]
+        assert isinstance(unit, str), "unit must be a string"
+        assert unit in units, f"unit not known, possible units: {units}"
         # CH1: w15, CH2: w16
         if unit == "V":
             amplitude = amplitude * 1000
@@ -147,18 +252,44 @@ class PSG9080:
         else:
             return False
         command = f":w1{4+channel}={amplitude}.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def offset(self, channel=1, offset=1):
+    def offset(self, channel=1, offset=1.00) -> bool:
+        """select offset
+
+        :param channel: desired channel, defaults to 1
+        :type channel: int, optional
+        :param offset: desired offset voltage, unit V, defaults to 1.00
+        :type offset: int, optional
+        :return: offset successfully selected (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
+        assert isinstance(offset, float), "offset must be int or float"
+        assert offset >= -9.99 and offset <= 15.00, "offset range: -9.99V to +15.00V"
         # offset range: -9.99V (offset_value = 1) to +15V (offset_value = 2500)
         # CH1: w17, CH2: w18
         offset_value = 100 * offset + 1000
         command = f":w1{6+channel}={offset_value}.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def duty_cycle(self, channel=1, duty_cycle=50):
+    def duty_cycle(self, channel=1, duty_cycle=50) -> bool:
+        """select duty cycle (only possible for some waveforms)
+
+        :param channel: desired channel, defaults to 1
+        :type channel: int, optional
+        :param duty_cycle: desired duty cycle, defaults to 50
+        :type duty_cycle: int, optional
+        :return: duty cycle successfully selected (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
+        assert isinstance(duty_cycle, float), "duty_cycle must be int or float"
+        assert (
+            duty_cycle >= 0.00 and duty_cycle <= 15.00
+        ), "duty cycle range: 0.00 to 99.99"
         # duty cycle range: 0.00 (0) to 99.99 (9999)
         # CH1: w19, CH2: w20
         if channel == 1:
@@ -167,27 +298,58 @@ class PSG9080:
             command = f":w20={duty_cycle * 100}.\n"
         else:
             return False
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def phase(self, channel=1, phase=0):
+    def phase(self, channel=1, phase=0) -> bool:
+        """select phase (only possible for some waveforms)
+
+        :param channel: desired channel, defaults to 1
+        :type channel: int, optional
+        :param phase: desired phase shift, defaults to 0
+        :type phase: int, optional
+        :return: phase successfully selected (True) or not (False)
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
+        assert isinstance(phase, float), "phase must be int or float"
+        assert phase >= 0.00 and phase <= 359.99, "phase range: 0.00 to 359.99"
         # phase range: 0° (0) to 359.99° (35999)
         # CH1: w21, CH2: w22
         command = f":w2{channel}={phase * 100}.\n"
-        self.__send(command)
-        return True
+        return self.__send(command)
 
-    def default(self, channel=1):
-        self.waveform(channel=channel, waveform="Sine")
-        self.frequency(channel=channel, frequency=100 * (10 ** channel), unit="Hz")
-        self.amplitude(channel=channel, amplitude=5, unit="V")
-        self.offset(channel=channel, offset=0)
-        self.duty_cycle(channel=channel, duty_cycle=50)
-        self.phase(channel=channel, phase=0)
-        self.off_all()
-        return True
+    def default(self, channel=1) -> bool:
+        """reset selected channel to default values
 
-    def default_all(self):
-        self.default(channel=1)
-        self.default(channel=2)
-        return True
+        :param channel: desired channel, defaults to 1
+        :type channel: int, optional
+        :return: channel successfully set to default
+        :rtype: bool
+        """
+        assert isinstance(channel, int), "channel must be int"
+        assert channel == 0 or channel == 1, "channel must be 1 or 2"
+        try:
+            self.waveform(channel=channel, waveform="Sine")
+            self.frequency(channel=channel, frequency=100 * (10 ** channel), unit="Hz")
+            self.amplitude(channel=channel, amplitude=5, unit="V")
+            self.offset(channel=channel, offset=0)
+            self.duty_cycle(channel=channel, duty_cycle=50)
+            self.phase(channel=channel, phase=0)
+            self.off_all()
+            return True
+        except:
+            return False
+
+    def default_all(self) -> bool:
+        """reset both channels to default values
+
+        :return: successfully set to default
+        :rtype: bool
+        """
+        try:
+            self.default(channel=1)
+            self.default(channel=2)
+            return True
+        except:
+            return False
